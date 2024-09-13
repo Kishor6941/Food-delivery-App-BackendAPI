80df2c422c7a028c360e5ff5d35ec08c4f20d4e7
Kishor6941 <phawadeks9049@gmail.com>
2024-09-13 19:20:16 +0530

    add remove get cartData

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js";
 // add config
 
 const app = express();
@@ -23,11 +24,7 @@ connectDB();
 app.use("/api/food",foodRouter);
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
-
-
-app.get('/',(req,res) => {
-    res.send("Welcome To Node JS");
-})
+app.use("/api/cart",cartRouter)
 
 app.listen(port,() => {
     console.log(`Server listening on port ${port}`)
